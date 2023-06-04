deae42396be0baf0f262cbae70fe7b2f5dcbb62f
AndreyJustino <100535840+AndreyJustino@users.noreply.github.com>
2023-06-04 15:07:23 -0300

    system resolutor part 8

--- src/pages/TabelaDenuncia/Tabela.jsx
+++ src/pages/TabelaDenuncia/Tabela.jsx
@@ -6,55 +6,84 @@ import { Link, useNavigate } from 'react-router-dom'
 import './Style.css'
 import api from '../api.js'
 
+import axios from 'axios'
+
 function Tabela() {
-
-    const [denuncia, setDenuncia] = useState([])
+    const [denuncia, setDenuncia] = useState([]);
+    const [enderecos, setEnderecos] = useState([]);
+  
     let navigate = useNavigate();
-
+  
     useEffect(() => {
-        async function getDenuncia() {
-            const {data} = await api.get('/resolutor')
-
-            setDenuncia(data) 
+      async function getDenuncia() {
+        const { data } = await api.get('/resolutor');
+        setDenuncia(data);
+  
+        if (data.length > 0) {
+          const enderecoPromises = data.map((item) => {
+            const latitude = item.latitude;
+            const longitude = item.longitude;
+  
+            const config = {
+              method: 'get',
+              url: `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&apiKey=ef172e5aac494f98ad94e03ba0d41fb8`,
+              headers: {},
+            };
+  
+            return axios(config)
+              .then(function (response) {
+                const endereco = response.data.features[0].properties.formatted;
+                return endereco;
+              })
+              .catch(function (error) {
+                console.log('Erro:', error);
+                return null;
+              });
+          });
+  
+          Promise.all(enderecoPromises).then((resultados) => {
+            setEnderecos(resultados);
+          });
         }
-        getDenuncia()
-    }, [denuncia])
-
-    // sessionStorage.setItem("id", denuncia[0].id_denuncia)
-
-  return (
-    <>
-        <Table striped bordered hover className='tabela'>
-            <thead>
-                <tr>
-                    <th>ID denuncia</th>
-                    <th>Quantidade</th>
-                    <th>Tipo de problema</th>
-                    <th>CEP</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    denuncia.map((item) => ( 
-                        
-                        <tr key={item.id_denuncia}>
-                            
-                            <td>{item.id_denuncia}</td>
-                            <td>{item.qnt_denuncia}</td>
-                            <td>{item.tipo_problema}</td>
-                            <td>{item.fk_cep}</td>
-                            
-                            <td>
-                                <button onClick={() => {navigate(`/forms/${item.id_denuncia}`)}} className='btn modal-trigger btn-editar'>Resolver</button>
-                            </td>
-
-                        </tr>
-                    ))
-                }
-            </tbody>
+      }
+  
+      getDenuncia();
+    }, []);
+  
+  
+    return (
+      <>
+        <Table striped bordered hover className="tabela">
+          <thead>
+            <tr>
+              <th>ID denuncia</th>
+              <th>Tipo de problema</th>
+              <th>Endereço</th>
+            </tr>
+          </thead>
+          <tbody>
+            {denuncia.map((item, index) => (
+              <tr key={item.id_denuncia}>
+                <td>{item.id_denuncia}</td>
+                <td>{item.tipo_problema}</td>
+                <td>{enderecos[index]}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      navigate(`/forms/${item.id_denuncia}`);
+                    }}
+                    className="btn modal-trigger btn-editar"
+                  >
+                    Resolver
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </Table>
-    </>
-  )
-}
-
-export default Tabela
+      </>
+    );
+  }
+  
+export default Tabela;
+  
